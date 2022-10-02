d4634cc7a88316b408c67473ac928ed9ab8ef1c3
ibrahimtoru <ibrahimtoru@gmail.com>
2022-10-02 12:48:48 +0500

    added snippets section, features section, access anywhere section and supercharge section

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,9 @@ module.exports = {
     },
     extend: {
       colors: {
-        strongCyan: "hsl(171,66%, 44%)",
+        strongCyan: "hsl(171, 66%, 44%)",
         lightBlue: "hsl(233, 100%, 69%)",
-        darkGarayishBlue: "hsl(210, 10%, 33%)",
+        darkGrayishBlue: "hsl(210, 10%, 33%)",
         grayishBlue: "hsl(201, 11%, 66%)",
       },
       fontFamily: {
